380b6f6e0646b1bfaceae306b74d340276dda6c4
Mohan <mohanprasanth3883@gmail.com>
2025-01-04 23:29:00 +0530

    initial commit

---
+++ public/Baby Care.js
@@ -0,0 +1,25 @@
+var sidenavbar=document.querySelector(".side-navbar-content");
+var menuicon=document.querySelector(".navbar-menu");
+function showNavbar(){
+    if(sidenavbar.style.display=="none"){
+    sidenavbar.style.display ="block";
+    }
+    else{
+        sidenavbar.style.display ="none";
+    }
+}
+showNavbar();
+
+window.addEventListener('scroll',function reveal(){
+    let reveals = this.document.querySelectorAll('.reavel');
+    for(let i = 0; i< reveals.length; i++){
+        let windowHeigth = this.window.innerHeight;
+        let revealtop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 100;
+        if(revealtop < windowHeigth-revealPoint){
+            reveals[i].classList.add('active');
+        }else{
+            reveals[i].classList.remove('active');
+        }
+    }
+});
